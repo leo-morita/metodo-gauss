2e4c7642f5b38245febdbb11f2d6153f4c13ef0a
leonardomorita <leonardomorita1@edu.unifil.br>
2019-09-27 19:09:57 -0300

    Atualização

--- src/br/com/calculoNumerico/gauss/view/Main.java
+++ src/br/com/calculoNumerico/gauss/view/Main.java
@@ -1,25 +1,83 @@
 package br.com.calculoNumerico.gauss.view;
 
-import br.com.calculoNumerico.gauss.controller.GaussController;
-import br.com.calculoNumerico.gauss.model.GaussModel;
-
-import java.util.Scanner;
-
 public class Main {
 
+    private static final double nuloNum = 1e-5;
+
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+        int TamanhoMatriz = 4;
+        double[][] A = {
+                { 1, 2, 3, 4},
+                { 2, 1, 2, 3},
+                { 3, 2, 1, 2},
+                { 4, 3, 2, 1}
+        };
 
-        System.out.println("Digite o valor do n: ");
-        int n = teclado.nextInt();
-        teclado.nextLine();
+        double[] igual = { 10, 7, 6, 5 };
 
-        GaussModel gaussModel = new GaussModel(n);
-        GaussController gaussController = new GaussController();
+        double[] x = LinearS(A, igual);
+        int num = 1;
 
-        gaussController.adicionarValores(gaussModel.getMatriz());
-//        gaussModel.imprimirMatriz();
-        gaussController.escalonar(gaussModel.getMatriz());
+        for (int i = 0; i < TamanhoMatriz; i++) {
+            System.out.printf("x" + num + " = ");
+            System.out.printf("%.4f ", x[i]);
+            System.out.printf("\n");
+            num++;
+        }
+    }
+
+    public static double[] LinearS(double[][] A, double[] b) {
+        int n = b.length;
+
+        for (int p = 0; p < n; p++) {
+
+            // Encontra a coluna Pivô e substitui
+            int max = p;
+            for (int i = p + 1; i < n; i++) {
+                if (Math.abs(A[i][p]) > Math.abs(A[max][p])) {
+                    max = i;
+                }
+            }
+            double[] temp = A[p]; A[p] = A[max]; A[max] = temp;
+            double   t    = b[p]; b[p] = b[max]; b[max] = t;
+
+            if (Math.abs(A[p][p]) <= nuloNum) {
+                throw new ArithmeticException("Determinante é 0");
+            }
+
+            for (int i = p + 1; i < n; i++) {
+                double alpha = A[i][p] / A[p][p];
+                b[i] -= alpha * b[p];
+                for (int j = p; j < n; j++) {
+                    A[i][j] -= alpha * A[p][j];
+                }
+            }
+
+            for (int i = 0; i < A.length; i++) {
+                for (int j = 0; j < A[i].length; j++) {
+                    if (j == A[i].length-1) {
+//                    System.out.printf("%-12d%-12d%d\n",15,12,5);
+                        System.out.printf("|%-12f%n", A[i][j]);
+//                    System.out.print(a[i][j] + "|\t");
+                    } else {
+                        System.out.printf("%-12f", A[i][j]);
+//                    System.out.print(a[i][j] + "\t\t");
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+        // Substituição de volta
+        double[] x = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
+            double sum = 0.0;
+            for (int j = i + 1; j < n; j++) {
+                sum += A[i][j] * x[j];
+            }
+            x[i] = (b[i] - sum) / A[i][i];
+        }
+        return x;
     }
 
 }
--- src/br/com/calculoNumerico/gauss/view/Main2.java
+++ src/br/com/calculoNumerico/gauss/view/Main2.java
@@ -12,7 +12,6 @@ public class Main2 {
         metodoEliminacaoGauss(a);
         /*System.out.printf("%-12s%-12s%s\n","Coluna 1","Coluna 2","Coluna 3");
         System.out.printf("%-12f%-12f%f\n",15.5556,12.5556,5.345666);*/
-
     }
 
     public static double[][] metodoEliminacaoGauss(double[][] a) {
@@ -38,38 +37,67 @@ public class Main2 {
 //                    System.out.print(a[i][j] + "\t");
                 }
                 System.out.println();
-                imprimi(a);
+                imprimir(a);
             }
             System.out.println("---------------------------");
         }
 
         System.out.println("Resultado");
-        imprimi(a);
+        imprimir(a);
         return null;
     }
 
-    public static void imprimi(double[][] a) {
-        int tamanho = a.length;
-        for (int i = 0; i < tamanho; i++) {
-            if (i == tamanho-1) {
-                System.out.printf("%s%n","b");
+    public static void imprimir(double[][] a) {
+        int indice = 0;
+        for (int i = 0; i < a.length; i++) {
+            indice = i + 1;
+            if (i == a.length-1) {
+                System.out.printf("%-12s", "x" + indice);
+                System.out.printf("|%-12s%n", "b");
+                /*System.out.println("Teste: " + "x" + i);
+                System.out.println("Teste: " + "| b");*/
             } else {
-                System.out.printf("%-12s", "x" + i);
+                System.out.printf("%-12s", "x" + indice);
+//                System.out.println("Teste: " + "x" + i);
             }
         }
 
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
-                if (j == a[i].length-2) {
+                if (j == a[i].length-1) {
 //                    System.out.printf("%-12d%-12d%d\n",15,12,5);
-//                    System.out.printf("|%-12f%n", a[i][j]);
-                    System.out.print(a[i][j] + "|\t");
+                    System.out.printf("|%-12f%n", a[i][j]);
+//                    System.out.print(a[i][j] + "|\t");
                 } else {
-//                    System.out.printf("%-12f", a[i][j]);
-                    System.out.print(a[i][j] + "\t\t");
+                    System.out.printf("%-12f", a[i][j]);
+//                    System.out.print(a[i][j] + "\t\t");
                 }
             }
-            System.out.println();
+            //System.out.println();
         }
+
+        /*double[] aux = new double[11];
+        double b = 0.0;
+        double[] resultante = new double[11];
+
+        for (int i = a.length-1; i >= 1; i--) {
+            System.out.println("Iteração: " + i);
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] > 0.0) {
+                    if (j == a[i].length-1) {
+                        b = a[i][j];
+                    } else {
+                        aux[j] = a[i][j];
+                    }
+                }
+                System.out.println(a[i][j]);
+            }
+            for (int j = 0; j < aux.length; j++) {
+                if (aux[j] > 0.0) {
+
+                }
+            }*/
+//        }
+
     }
 }
