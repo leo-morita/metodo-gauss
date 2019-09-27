cdfa8f4918a559176ba5c291d17b08765587401b
leonardomorita <leonardomorita1@edu.unifil.br>
2019-09-27 11:01:07 -0300

    Atualização

--- src/br/com/calculoNumerico/gauss/controller/GaussController.java
+++ src/br/com/calculoNumerico/gauss/controller/GaussController.java
@@ -18,6 +18,7 @@ public class GaussController {
     }
 
     public void escalonar(String[][] matriz) {
+        // Fase da eliminação
         System.out.println("Etapa K=0");
 
         for (int linha = 0; linha < matriz.length; linha++) {
@@ -26,6 +27,47 @@ public class GaussController {
             }
             System.out.println();
         }
+
+        // Determinar o pivô
+        int[][] auxiliar = new int[matriz.length][matriz[0].length];
+        int indicePivo = 0;
+
+        for (int coluna = 0; coluna < matriz[0].length; coluna++) {
+            for (int linha = 0; linha < matriz.length; linha++) {
+                if (linha == 0) {
+                    System.out.println(matriz[coluna][linha]);
+                }
+            }
+        }
+
+        /*for (int linha = 0; linha < matriz.length; linha++) {
+            String pivo = "";
+            int pivoAtual = 0;
+            for (int coluna = 0; coluna < matriz[0].length; coluna++) {
+                if (indicePivo < matriz.length-1) {
+                    pivo = matriz[linha][indicePivo];
+                    pivoAtual = Integer.parseInt(pivo.split("x")[0]);
+                }
+
+                *//*int elementoAtual = Integer.parseInt(matriz[linha+indicePivo][coluna].split("x")[0]);
+                int valorAtual = (((elementoAtual / pivoAtual) * pivoAtual) - elementoAtual);
+                System.out.print("[Pivô: " + pivoAtual + " ");
+                System.out.print("Elemento Atual: " + elementoAtual + " ");
+                System.out.println("Novo Valor: " + valorAtual + "]");
+                auxiliar[linha][coluna] = valorAtual;*//*
+            }
+            if (indicePivo < matriz.length-1) {
+                System.out.println(pivoAtual);
+            }
+            indicePivo += 1;
+        }*/
+
+        /*for (int linha = 0; linha < auxiliar.length; linha++) {
+            for (int coluna = 0; coluna < auxiliar[0].length; coluna++) {
+                int elementoAtual = auxiliar[linha][coluna];
+                System.out.println(elementoAtual);
+            }
+        }*/
     }
 
 }
