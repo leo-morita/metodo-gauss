f6fcddbaa079dc9c509b47fd03080162fc72048e
Laboratório Informática <labinfo@unifil.br>
2019-10-18 22:10:33 -0300

    atualização

--- src/br/com/calculoNumerico/gauss/view/Main.java
+++ src/br/com/calculoNumerico/gauss/view/Main.java
@@ -1,7 +1,6 @@
-
+package br.com.calculoNumerico.gauss.view;
 
 public class Main {
-
     private static final double nuloNum = 1e-5;
 
     public Main(int tamanhoM, double[][] m, double[] ig){
@@ -24,28 +23,27 @@ public class Main {
     public static void main(String[] args) {
         int TamanhoMatriz = 4;
         double[][] A = {
-                { 1, 2, 3, 4},
-                { 2, 1, 2, 3},
-                { 3, 2, 1, 2},
-                { 4, 3, 2, 1}
+                {1, 2, 3, 4},
+                {2, 1, 2, 3},
+                {3, 2, 1, 2},
+                {4, 3, 2, 1}
         };
 
-        double[] igual = { 10, 7, 6, 5 };
+        double[] igual = {10, 7, 6, 5};
 
         double[] x = LinearS(A, igual);
         int num = 1;
 
-        for (int i = 0; i < TamanhoMatriz; i++) {
+        /*for (int i = 0; i < TamanhoMatriz; i++) {
             System.out.printf("x" + num + " = ");
             System.out.printf("%.4f ", x[i]);
             System.out.printf("\n");
             num++;
-        }
+        }*/
     }
 
     public static double[] LinearS(double[][] A, double[] b) {
         int n = b.length;
-
         for (int p = 0; p < n; p++) {
             System.out.println("K = " + p);
 
@@ -63,37 +61,24 @@ public class Main {
                 throw new ArithmeticException("Determinante é 0");
             }
 
+            // Teste
+            for (int i = 0; i < temp.length; i++) {
+                System.out.print(temp[i] + " ");
+            }
+            System.out.println();
+
             for (int i = p + 1; i < n; i++) {
                 double alpha = A[i][p] / A[p][p];
+
+                // Teste
+                System.out.println("Teste: Valor = " + alpha);
+
                 b[i] -= alpha * b[p];
                 for (int j = p; j < n; j++) {
                     A[i][j] -= alpha * A[p][j];
                 }
             }
-            int indice;
-            /*for (int i = 0; i < A.length; i++) {
-                indice = i + 1;
-                if (i == A.length-1) {
-                    System.out.printf("%-12s", "x" + indice);
-                    System.out.printf("|%-12s%n", "b");
-                } else {
-                    System.out.printf("%-12s", "x" + indice);
-                }
-            }*/
-
-            for (int i = 0; i < A.length; i++) {
-                for (int j = 0; j < A[i].length; j++) {
-                    if (j == A[i].length-1) {
-//                    System.out.printf("%-12d%-12d%d\n",15,12,5);
-                        System.out.printf("%-12f", A[i][j]);
-                        System.out.printf("|%-12f%n", b[i]);
-//                    System.out.print(a[i][j] + "|\t");
-                    } else {
-                        System.out.printf("%-12f", A[i][j]);
-//                    System.out.print(a[i][j] + "\t\t");
-                    }
-                }
-            }
+            imprimirMatriz(A, b);
             System.out.println();
         }
 
@@ -107,6 +92,19 @@ public class Main {
             x[i] = (b[i] - sum) / A[i][i];
         }
         return x;
+    }
+
+    private static void imprimirMatriz(double[][] A, double[] b) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                if (j == A[i].length-1) {
+                    System.out.printf("%-12f", A[i][j]);
+                    System.out.printf("|%-12f%n", b[i]);
+                } else {
+                    System.out.printf("%-12f", A[i][j]);
+                }
+            }
+        }
     }
 
 }
--- src/br/com/calculoNumerico/gauss/view/Tela.java
+++ src/br/com/calculoNumerico/gauss/view/Tela.java
@@ -1,4 +1,4 @@
-/*
+package br.com.calculoNumerico.gauss.view;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,6 +8,7 @@
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import javafx.scene.control.TableColumn;
+import br.com.calculoNumerico.gauss.view.Main;
 
 /**
  *
